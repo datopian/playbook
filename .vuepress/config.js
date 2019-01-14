c81b874294f11aaa64bd7ad354d369a7d1fcdf8d
Rufus Pollock <rufus.pollock@okfn.org>
2019-01-14 14:20:14 +0000

    [theme][s]: link to okrs and culture from navbar.

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -21,12 +21,15 @@ module.exports = {
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Guide', link: '/guide/' },
+      { text: 'Culture', link: '/culture/' },
       { text: 'Onboarding', link: '/onboarding/' },
       { text: 'Agile Delivery', link: '/agile/' },
       { text: 'User Stories', link: '/user-stories/' },
       {
         text: 'Howtos',
         items: [
+          { text: 'Objectives and Key Results (OKRs)', link: '/objectives-and-key-results/' },
+          { text: 'Getting Things Done (GTD)', link: '/getting-things-done/' },
           { text: 'Planning Your Day', link: '/planning-your-day/' },
           { text: 'Markdown', link: '/markdown/' },
           { text: 'SCQH (Situation, Complication, Question, Hypothesis)', link: '/scqh/' }
